b705363f6ce9de2802afde1403b177662e9395c2
beki-get <bereketgetayea@gmail.com>
2026-08-04 21:03:40 +0300

    update image validation

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,9 +2,13 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
 };
+
 export default nextConfig;
